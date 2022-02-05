36f118d4e2ededc4af103607797cca919685e811
kousuke.tamogami <okasuke.0714@gmial.com>
2022-02-05 08:36:19 +0000

    model

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -16,9 +16,9 @@
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
-                            {{-- ユーザ詳細ページへのリンク --}}
-                            <li class="dropdown-item"><a href="#">My account</a></li>
-                            <li class="dropdown-divider"></li>
+                            <!--{{-- ユーザ詳細ページへのリンク --}}-->
+                            <!--<li class="dropdown-item"><a href="#">My account</a></li>-->
+                            <!--<li class="dropdown-divider"></li>-->
                             {{-- ログアウトへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
                         </ul>
